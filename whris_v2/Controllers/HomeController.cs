f61ada4692450ee018a696119ec1b8755ac32ccb
Erwin Wilson Ceniza <erwinwilsonceniza@outlook.com>
2019-12-25 16:31:03 +0800

    Add project files.

---
+++ whris_v2/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using whris_v2.Data;
+
+namespace whris_v2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public whrisDataContext whris;
+        public JsonResult GetUserList()
+        {
+            whris = new whrisDataContext();
+
+            var userList = whris.MstUsers
+                .Select(i => new Models.MstUser
+                {
+                    Id = i.Id,
+                    Username = i.UserName,
+                    FullName = i.FullName,
+                    IsLocked = i.IsLocked
+                });
+
+            return Json(userList.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ whris_v2/Models/MstUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace whris_v2.Models
+{
+    public class MstUser
+    {
+        public Int64 Id { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public Boolean IsLocked { get; set; }
+        public Int64 EntryUserId { get; set; }
+        public DateTime EntryDateTime { get; set; }
+        public Int64 UpdateUserId { get; set; }
+        public DateTime UpdateDateTime { get; set; }
+    }
+}
